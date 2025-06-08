2120640432df7644f78176d7e0bfc0b2989e05d8
Sumit Rana <118682671+Sumit-rana13@users.noreply.github.com>
2025-06-08 21:22:32 +0530

    modified file

--- backend/utils/helper.js
+++ backend/utils/helper.js
@@ -1,11 +1,3 @@
-// module.exports = function formatDate(date) {
-//   const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
-//                   "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-//   return `${date.getDate().toString().padStart(2, '0')}-${months[date.getMonth()]}-${date.getFullYear()}`;
-// };
-
-
-// // /backend/utils/helpers.js
 
 export default function formatDate(date) {
   const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
